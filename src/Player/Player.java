bbc563950bb424145252eec4da0febe9fdfa4b32
dehanalkautsar <dehanalkautsar3@gmail.com>
2021-04-16 09:08:10 +0700

    create player.java masi error

---
+++ src/Player/Player.java
@@ -0,0 +1,16 @@
+package Player;
+import java.util.*;
+
+public class Player {
+     //atribut
+     protected Inventory<Engimon> inventoryEngimon;
+     protected Inventory<SkillItem> inventorySkill;
+     protected Engimon activeEngimon;
+     protected int MaxCapacity = 30;
+
+     //constructor
+     Player(String starter_name, int species) {
+          Engimon starterEngimon = //buat engimon di factory;
+          
+     }
+}
